d6b2bc2c0d64c9a09aafbb33a4bf250c0a2e7d9d
Zeng <alan.zeng@cgi.com>
2018-06-19 21:36:55 -0400

    https://github.com/alanzeng1213/sprints

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+import { SprintsComponent } from './components/sprints/sprints.component';
+
+const routes: Routes = [
+
+  {path: 'sprints' , component: SprintsComponent},
+  {path: 'profile' , component: ProfileComponent},
+  {path: '' , component: HomeComponent},
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { CallbackComponent } from './components/callback/callback.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AuthService } from './components/auth/auth.service';
+import { SprintsComponent } from './components/sprints/sprints.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    AuthComponent,
+    CallbackComponent,
+    HeaderComponent,
+    SprintsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
